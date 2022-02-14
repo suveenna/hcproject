949e8720194eae95cca63b6c7d0128a04a11754e
suveenna <suveenamsn@gmail.com>
2022-02-14 00:39:11 -0800

    ALLOWED HOSTher2

--- hand_craft/urls.py
+++ hand_craft/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('appname/',include('craft_shop.urls'))
+    path('',include('craft_shop.urls'))
 ]
