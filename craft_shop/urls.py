1d55c9f995a95c8785940ed2dc35e2319b2c7988
suveenna <suveenamsn@gmail.com>
2022-02-13 23:33:43 -0800

    updatinghero

--- craft_shop/urls.py
+++ craft_shop/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
 
   path('home',views.home)
 
+
 ]
--- hand_craft/settings.py
+++ hand_craft/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,4 +127,4 @@ STATIC_ROOT = '/craft_shop/static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFileStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
