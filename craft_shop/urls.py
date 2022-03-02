7be46ca307f14712a1f53adbdc711ab0453a1bb2
suveenna <suveenamsn@gmail.com>
2022-03-01 20:31:44 -0800

    design1

--- craft_shop/urls.py
+++ craft_shop/urls.py
@@ -4,7 +4,20 @@ from . import views
 urlpatterns=[
 
   path('',views.home, name="home"),
-  path('login',views.login, name="login")
+  path('login',views.login, name="login"),
+  path('signup',views.signup, name="signup"),
+  path('cart',views.cart, name="cart"),
+  path('index',views.index, name="index"),
+  path('dining',views.dining, name="dining"),
+  path('decor',views.decor, name="decor"),
+  path('master',views.master, name="master"),
+  path('dress',views.dress, name="dress"),
+  path('garden',views.garden, name="garden"),
+  path('gift',views.gift, name="gift"),
+  path('lighting',views.lighting, name="lighting")
+
+
+
 
 
 ]
--- craft_shop/views.py
+++ craft_shop/views.py
@@ -8,3 +8,33 @@ def home(request):
      
 def login(request):
     return render(request,'login.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def cart(request):
+    return render(request,'cart.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def dining(request):
+    return render(request,'dining.html')
+
+def decor(request):
+    return render(request,'decor.html')
+
+def master(request):
+    return render(request,'master.html')
+
+def dress(request):
+    return render(request,'dress.html')    
+
+def garden(request):
+    return render(request,'garden.html')
+
+def gift(request):
+    return render(request,'gift.html')
+
+def lighting(request):
+    return render(request,'lighting.html')              
